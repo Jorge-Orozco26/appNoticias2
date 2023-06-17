d08a52eac1affb478b2ef77eb641a0782daeae64
Jorge Orozco <creneorozco@gmail.com>
2023-06-17 17:40:55 -0600

    Componente App

---
+++ App.js
@@ -0,0 +1,28 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+import NewsList from './src/NewsList';
+import NewsDetail from './src/NewsDetail';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name= 'NewsList'
+                    component={NewsList}
+                    options={{ title: 'Últimas noticias' }}
+                />
+                <Stack.Screen
+                    name="NewsDetail"
+                    component={NewsDetail}
+                    options={{ title: 'Detalle de noticias'}}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default App;
